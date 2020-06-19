e136af51cb19941c12a53b425824160b7591fb75
Jessica Wolff <jleahwolff@gmail.com>
2020-06-19 11:26:56 -0500

    changes to recursive count

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -13,8 +13,17 @@ def count_th(word):
         if word[0] == 't' and word[1] == 'h':
             #add that word to the counter
             counter += 1
+            #return the counter # starting 
             return counter + count_th(word[1:])
+            #print it to see if its working
+            print(word, "Counter is:", counter)
         else:
             return count_th(word[1:])
     else:
         return counter
+
+count_th("thhoweuwoeuthwoefnwoethnowe")
+count_th("hwkjfhkwejhkwjehcwjeth")
+count_th("ththththtththththththth")
+
+#passing :) 
